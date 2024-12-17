d568d402abde031fadf9226fd159426919ef56b2
Nazar Solonovich <soln4r@gmail.com>
2024-12-17 20:58:04 +0300

    Implement the Insertion Sort algorithm to sort a dynamic array of objects based on their price. The dynamic array is managed using std::unique_ptr for safe memory management.

--- CppLearning/CppLearning.cpp
+++ CppLearning/CppLearning.cpp
@@ -1,7 +1,7 @@
 ﻿/*
  *	Objective:
- *	Learn to manage dynamic memory safely and
- *	efficiently using std::unique_ptr for arrays of objects.
+ *	Implement the Insertion Sort algorithm to sort a dynamic array of objects based
+ *	on their price. The dynamic array is managed using std::unique_ptr for safe memory management.
  */
 
 #include <iostream>
@@ -19,19 +19,21 @@ public:
 		name(product_name), price(product_price) {
 	}
 
+	double GetPrice() const
+	{
+		return price;
+	}
+
 	void PrintInfo() const
 	{
 		std::cout << "Product: " << name << " | Price: " << price << std::endl;
 	}
-
-	~Product() {}
 };
 
 class ProductManagerSmart
 {
 private:
 
-	// Product* products;
 	std::unique_ptr<Product[]> products;
 	int array_size;
 
@@ -40,55 +42,82 @@ public:
 	ProductManagerSmart(const int size) : array_size(size)
 	{
 		if (size <= 0) throw std::invalid_argument("Size of the array must be greater than 0!");
-		//products = new Product[array_size];
 		products = std::make_unique<Product[]>(array_size);
 	}
 
 	void AddProduct(int index, const std::string& name, double price)
 	{
+		if (products == nullptr) throw std::logic_error("Products array is not initialized.");
 		if (index < 0 || index >= array_size) throw std::range_error("Going outside the array!");
 		products[index] = Product(name, price);
 	}
 
-	void PrintAllProducts() const
+	void PrintAllProducts(bool ascending = true) const
 	{
-		std::cout << "Product list:" << std::endl;
+		std::cout << "Product list (sorted in " << (ascending ? "ascending" : "descending") << " order):" << std::endl;
 		for (int i = 0; i < array_size; i++)
 		{
 			products[i].PrintInfo();
 		}
 	}
+	
+	void InsertionSortByPrice(bool ascending = true)
+	{
+		if (!products) throw std::logic_error("The array is empty");
+		for (int i = 1; i < array_size; i++)
+		{
+			Product key = products[i];
+			int j = i - 1;
 
-	~ProductManagerSmart() {
-		//delete[] products;
+			while (j >= 0 && 
+				(ascending ? products[j].GetPrice() > key.GetPrice()
+				: products[j].GetPrice() < key.GetPrice()))
+			{
+				products[j + 1] = products[j];
+				j--;
+			}
+			products[j + 1] = key;
+		}
 	}
-
 };
 
 int main()
 {
-	ProductManagerSmart product_list(3);
+	ProductManagerSmart product_list(5);
 
 	try
 	{
-		product_list.AddProduct(0, "Apple", 1.5);
-		product_list.AddProduct(1, "Banana", 0.9);
-		product_list.AddProduct(2, "Orange", 1.2);
+		product_list.AddProduct(0, "Pineapple", 2.5);
+		product_list.AddProduct(1, "Apple", 1.5);
+		product_list.AddProduct(2, "Banana", 0.9);
+		product_list.AddProduct(3, "Mango", 3.0);
+		product_list.AddProduct(4, "Orange", 1.8);
+
+		product_list.InsertionSortByPrice(true);
+		product_list.PrintAllProducts(true);
+
+		std::cout << std::endl;
+
+		product_list.InsertionSortByPrice(false);
+		product_list.PrintAllProducts(false);
+
+		
 	}
 	catch (const std::invalid_argument& e)
 	{
-		std::cerr << "Invalid argiment: " << e.what() << std::endl;
+		std::cerr << "Invalid argument: " << e.what() << std::endl;
 	}
 	catch (const std::range_error& e)
 	{
 		std::cerr << "Range error: " << e.what() << std::endl;
 	}
+	catch (const std::logic_error& e)
+	{
+		std::cerr << "Logic error: " << e.what() << std::endl;
+	}
 	catch (...)
 	{
 		std::cerr << "An unknown error occurred." << std::endl;
 	}
-
-	product_list.PrintAllProducts();
-
 	return 0;
 }
