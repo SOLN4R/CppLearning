59e1c0075f8a53ef1cd28752fd60e34fdfa9c340
Nazar Solonovich <soln4r@gmail.com>
2024-12-17 23:55:41 +0300

    Implement two fundamental data structures, Stack and Queue, using dynamic arrays managed by std::unique_ptr.

--- CppLearning/CppLearning.cpp
+++ CppLearning/CppLearning.cpp
@@ -1,123 +1,151 @@
 ﻿/*
  *	Objective:
- *	Implement the Insertion Sort algorithm to sort a dynamic array of objects based
- *	on their price. The dynamic array is managed using std::unique_ptr for safe memory management.
+ *	Implement two fundamental data structures, Stack and Queue,
+ *	using dynamic arrays managed by std::unique_ptr.
  */
 
 #include <iostream>
 
-class Product
+class Stack // LIFO (Last In, First Out)
 {
 private:
 
-	std::string name;
-	double price;
+	std::unique_ptr<int[]> data;
+	int capacity; // the maximum stack capacity
+	int top; // is the index of the top element of the stack
 
 public:
 
-	Product(const std::string product_name = "Unknow", const double product_price = 0.0) :
-		name(product_name), price(product_price) {
-	}
-
-	double GetPrice() const
+	Stack(const int size) : capacity(size), top(-1)
 	{
-		return price;
+		data = std::make_unique<int[]>(capacity);
 	}
-
-	void PrintInfo() const
+	bool IsEmpty() const
 	{
-		std::cout << "Product: " << name << " | Price: " << price << std::endl;
+		return top < 0;
+	}
+	bool IsFull() const
+	{
+		return top + 1 == capacity;
+	}
+	void Push(const int value)
+	{
+		
+		if (IsFull()) throw std::overflow_error("The stack is full.");
+		data[++top] = value;
+	}
+	int Pop()
+	{
+		if (IsEmpty()) throw std::underflow_error("The stack is empty.");
+		return data[top--];
+	}
+	
+	void Print(const std::string& message = "Stack elements:\t\t") const
+	{
+		if (IsEmpty()) throw std::underflow_error("The stack is empty.");
+		std::cout << message;
+		for (int i = 0; i <= top; i++)
+		{
+			std::cout << data[i] << " ";
+		}
+		std::cout << std::endl;
 	}
 };
 
-class ProductManagerSmart
+class Queue // FIFO (First In, First Out)
 {
 private:
 
-	std::unique_ptr<Product[]> products;
-	int array_size;
+	std::unique_ptr<int[]> data;
+	int capacity;
+	int front;		// is the index of the first element of the queue
+	int rear;		// is the index of the last element of the queue.
+	int size;		// the current queue size
 
 public:
-
-	ProductManagerSmart(const int size) : array_size(size)
+	Queue(const int capacity) : capacity(capacity), front(0), rear(0), size(0)
 	{
-		if (size <= 0) throw std::invalid_argument("Size of the array must be greater than 0!");
-		products = std::make_unique<Product[]>(array_size);
+		data = std::make_unique<int[]>(capacity);
 	}
 
-	void AddProduct(int index, const std::string& name, double price)
+	bool IsFull() const
 	{
-		if (products == nullptr) throw std::logic_error("Products array is not initialized.");
-		if (index < 0 || index >= array_size) throw std::range_error("Going outside the array!");
-		products[index] = Product(name, price);
+		return size == capacity;
+	}
+	bool IsEmpty() const
+	{
+		return !size;
+	}
+	void Enqueue(int value)
+	{
+		if (IsFull()) throw std::overflow_error("The queue is full.");
+		data[rear] = value;
+		rear = (rear + 1) % capacity;
+		size++;
+	}
+	int Dequeue()
+	{
+		if (IsEmpty()) throw std::overflow_error("The queue is empty.");
+		int value = data[front];
+		front = (front + 1) % capacity;
+		size--;
+		return value;
 	}
 
-	void PrintAllProducts(bool ascending = true) const
+	void Print(const std::string& message = "Queue elements:\t\t") const
 	{
-		std::cout << "Product list (sorted in " << (ascending ? "ascending" : "descending") << " order):" << std::endl;
-		for (int i = 0; i < array_size; i++)
+		if (IsEmpty()) throw std::underflow_error("The queue is empty.");
+		std::cout << message;
+		for (int i = 0; i < size; i++)
 		{
-			products[i].PrintInfo();
+			int index = (front + i) % capacity;
+			std::cout << data[index] << " ";
 		}
-	}
-	
-	void InsertionSortByPrice(bool ascending = true)
-	{
-		if (!products) throw std::logic_error("The array is empty");
-		for (int i = 1; i < array_size; i++)
-		{
-			Product key = products[i];
-			int j = i - 1;
-
-			while (j >= 0 && 
-				(ascending ? products[j].GetPrice() > key.GetPrice()
-				: products[j].GetPrice() < key.GetPrice()))
-			{
-				products[j + 1] = products[j];
-				j--;
-			}
-			products[j + 1] = key;
-		}
+		std::cout << std::endl;
 	}
 };
 
 int main()
 {
-	ProductManagerSmart product_list(5);
+	Stack stack(5);
 
-	try
-	{
-		product_list.AddProduct(0, "Pineapple", 2.5);
-		product_list.AddProduct(1, "Apple", 1.5);
-		product_list.AddProduct(2, "Banana", 0.9);
-		product_list.AddProduct(3, "Mango", 3.0);
-		product_list.AddProduct(4, "Orange", 1.8);
+	stack.Push(1);
+	stack.Push(2);
+	stack.Push(3);
+	stack.Push(4);
+	stack.Push(5);
 
-		product_list.InsertionSortByPrice(true);
-		product_list.PrintAllProducts(true);
+	stack.Print();
 
-		std::cout << std::endl;
+	stack.Pop();
+	stack.Print("Stack after pop:\t");
 
-		product_list.InsertionSortByPrice(false);
-		product_list.PrintAllProducts(false);
+	stack.Pop();
+	stack.Print("Stack after pop:\t");
+	
+	stack.Push(9);
+	stack.Print("Stack after push (9):\t");
 
-		
-	}
-	catch (const std::invalid_argument& e)
-	{
-		std::cerr << "Invalid argument: " << e.what() << std::endl;
-	}
-	catch (const std::range_error& e)
-	{
-		std::cerr << "Range error: " << e.what() << std::endl;
-	}
-	catch (const std::logic_error& e)
-	{
-		std::cerr << "Logic error: " << e.what() << std::endl;
-	}
-	catch (...)
-	{
-		std::cerr << "An unknown error occurred." << std::endl;
-	}
+	std::cout << std::endl;
+
+	Queue queue(5);
+
+	queue.Enqueue(1);
+	queue.Enqueue(2);
+	queue.Enqueue(3);
+	queue.Enqueue(4);
+	queue.Enqueue(5);
+
+	queue.Print();
+
+	queue.Dequeue();
+	queue.Print("Queue after Dequeue:\t");
+
+	queue.Dequeue();
+	queue.Print("Queue after Dequeue:\t");
+
+	queue.Enqueue(9);
+	queue.Print("Queue after Enqueue:\t");
+
 	return 0;
 }
