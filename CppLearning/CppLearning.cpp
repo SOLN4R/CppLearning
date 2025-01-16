1b028e699ffebbef5d9b5241f0411cf5eb05977a
SOLN4R <soln4r@gmail.com>
2025-01-16 17:20:34 +0300

    Basics of OOP: constructors

--- CppLearning/CppLearning.cpp
+++ CppLearning/CppLearning.cpp
@@ -1,78 +1,47 @@
 ﻿/*
- *	 Basics of OOP: creating classes, objects and encapsulation
+ *	 Basics of OOP: constructors
  */
 
 #include <iostream>
 
 class Character
 {
-public:
-	std::string name;
 private:
-	int _health = 100;
-	int _level = 1;
+	std::string _name;
+	int _health;
+	int _level;
 
 public:
-	void TakeDamage(int damage)
+	Character(const std::string& name, int health, int level) : _name(name), _health(health), _level(level) 
 	{
-		if (damage <= 0) {
-			std::cout << "[Error] The damage must be greater than 0." << std::endl;
-			return;
-		}
-
-		_health = std::max(0, _health -= damage);
-		
-		if (_health <= 0) {
-			std::cout << "[Info] " << name << "'s character is destroyed." << std::endl;
-		}
-		else {
-			std::cout << "[Info] " << name << "'s character took damage of " << damage << "." << std::endl;
-		}
-	}
-
-	void Heal(int amount)
-	{
-		if (amount <= 0) {
-			std::cout << "[Error] The heal amount must be greater than 0." << std::endl;
-			return;
-		}
-
-		_health = std::min(100, _health += amount);
-		std::cout << "[Info] " << name << "'s character's health has been increased to " << _health << "." << std::endl;
-	}
-	
-	void DisplayStatus()
-	{
-		if (name.empty()) {
-			std::cout << "[Error] Name cannot be empty!" << std::endl;
-			return;
-		}
-
-		std::cout << "Character:" << std::endl;
-		std::cout << "Name: " << name << std::endl;
+		std::cout << "Character " << _name << " created:" << std::endl;
 		std::cout << "Health: " << _health << std::endl;
 		std::cout << "Level: " << _level << std::endl;
+	}
+
+	Character(const std::string& name) : _name(name)
+	{
+		_health = 100;
+		_level = 1;
+
+		std::cout << "Character " << _name << " created:" << std::endl;
+		std::cout << "Health: " << _health << std::endl;
+		std::cout << "Level: " << _level << std::endl;
+	}
+
+	/*
+	* Character(const std::string& name, int health = 100, int level = 1) : _name(name), _health(health), _level(level) { ... }
+	*/
+
+	~Character()
+	{
+		std::cout << "Character " << _name << " destroyed." << std::endl;
 	}
 };
 
 int main()
 {
-	Character warrior;
-	Character magician;
-
-	warrior.DisplayStatus();
-
-	warrior.name = "Warrior";
-	magician.name = "Magician";
-
-	warrior.DisplayStatus();
-	warrior.TakeDamage(20);
-	warrior.Heal(10);
-	warrior.DisplayStatus();
-
-	magician.DisplayStatus();
-	magician.TakeDamage(50);
-	magician.Heal(20);
-	magician.DisplayStatus();
+	Character warrior("Warrior");
+	Character magician("Magician", 80, 1);
 	return 0;
 }
