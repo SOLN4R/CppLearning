0c89918eb6b15a423362c4ade8c9497feeb49e6e
SOLN4R <soln4r@gmail.com>
2025-01-17 22:20:58 +0300

    Stack

--- CppLearning/CppLearning.cpp
+++ CppLearning/CppLearning.cpp
@@ -1,42 +1,118 @@
 ﻿/*
- *	Templates: Function and Сlass
+ *	Stack (LIFO: Last In, First Out)
  */
 
 #include <iostream>
 
-// Function
-template <typename T>
-T Add(T a, T b)
-{
-	return a + b;
-}
-
-// Class
-template <typename T>
-class Box
+template<typename T>
+class Stack
 {
 private:
-	T item;
+	T* _data;
+	int _top_index;
+	int _capacity;
+
 public:
-	void SetItem(const T& new_item)
+	Stack(int capacity) : _capacity(capacity), _top_index(-1)
 	{
-		item = new_item;
+		_data = new T[_capacity];
+		std::cout << "[Core] A stack with a capacity of " << capacity << " has been created." << std::endl;
 	}
 
-	T GetItem() const
+	~Stack()
 	{
-		return item;
+		delete[] _data;
+		_data = nullptr;
+		std::cout << "[Core] The stack has shut down and cleared the memory." << std::endl;
+	}
+
+	bool isFull() const
+	{
+		return _top_index == _capacity - 1;
+	}
+
+	bool isEmpty() const
+	{
+		return _top_index == -1;
+	}
+
+	void Push(const T& value)
+	{
+		if (isFull()) {
+			std::cout << "[Error] Stack overflow." << std::endl;
+			return;
+		}
+		_data[++_top_index] = value;
+		std::cout << "[Info] Added element: " << value << std::endl;
+	}
+
+	void Pop()
+	{
+		if (isEmpty()) {
+			std::cout << "[Error] Stack is empty." << std::endl;
+			return;
+		}
+		std::cout << "[Info] Element deleted: " << _data[_top_index] << std::endl;
+		_top_index--;
+	}
+
+	T Top() const
+	{
+		if (isEmpty()) throw std::out_of_range("[Error] Stack is empty.");
+		return _data[_top_index];
+	}
+
+	void Clear()
+	{
+		_top_index = -1;
+		std::cout << "[Info] Stack has been cleared." << std::endl;
+	}
+
+	void Print() const
+	{
+		if (isEmpty())
+		{
+			std::cout << "[Info] Stack is empty." << std::endl;
+			return;
+		}
+		std::cout << "Stack elements:";
+		for (int i = 0; i <= _top_index; i++)
+		{
+			std::cout << " " << _data[i];
+		}
+		std::cout << std::endl;
 	}
 };
 
 int main()
 {
-	// Function
-	std::cout << Add<int>(10, 8) << std::endl;
+	Stack<int> numbers(5);
+	try {
+		std::cout << "Top element: " << numbers.Top() << std::endl;
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	numbers.Push(1);
+	numbers.Push(9);
+	numbers.Push(18);
+	numbers.Push(27);
+	numbers.Print();
+	numbers.Pop();
+	numbers.Pop();
+	numbers.Print();
+	std::cout << "Top element: " << numbers.Top() << std::endl;
 
-	// Class
-	Box<std::string> box;
-	box.SetItem("Chocolate");
-	std::cout << "The box contains: " << box.GetItem() << std::endl;
+	std::cout << std::endl;
+
+	Stack<std::string> spells(5);
+	spells.Push("Tornado");
+	spells.Push("Cold Snap");
+	spells.Push("Sun Strike");
+	spells.Push("Chaos Meteor");
+	spells.Push("Deafening Blast");
+	spells.Print();
+	spells.Clear();
+	spells.Print();
 	return 0;
 }	
